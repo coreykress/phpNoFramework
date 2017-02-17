eab567c93f18bd9e726b3ab988f651381deedabc
Corey Kress <coreykre@buffalo.edu>
2017-02-17 00:10:53 -0500

    added error handling

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -1,3 +1,26 @@
 <?php declare(strict_types = 1);
 
+namespace src;
+
+use Whoops\Handler\PrettyPageHandler;
+use Whoops\Run;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+error_reporting(E_ALL);
+
+$environment = 'development';
+
+$whoops = new Run();
+if ($environment !== 'production') {
+    $whoops->pushHandler(new PrettyPageHandler());
+} else {
+    $whoops->pushHandler(function ($e) {
+        echo 'error page';
+    });
+}
+$whoops->register();
+
+throw new \Exception();
+
 echo 'Hello World!';
