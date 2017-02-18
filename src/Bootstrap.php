eef74fd089edb878ca63617ec5ae9c4a9f73560f
Corey Kress <coreykre@buffalo.edu>
2017-02-17 19:38:37 -0500

    added routing and test routes

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -2,6 +2,10 @@
 
 namespace src;
 
+use FastRoute\Dispatcher;
+use FastRoute\RouteCollector;
+use Http\HttpRequest;
+use Http\HttpResponse;
 use Whoops\Handler\PrettyPageHandler;
 use Whoops\Run;
 
@@ -11,6 +15,7 @@ error_reporting(E_ALL);
 
 $environment = 'development';
 
+//error handler
 $whoops = new Run();
 if ($environment !== 'production') {
     $whoops->pushHandler(new PrettyPageHandler());
@@ -21,6 +26,39 @@ if ($environment !== 'production') {
 }
 $whoops->register();
 
-throw new \Exception();
+//http
+$request = new HttpRequest($_GET, $_POST, $_COOKIE, $_FILES, $_SERVER);
+$response = new HttpResponse();
 
-echo 'Hello World!';
+foreach ($response->getHeaders() as $header) {
+    header($header, false);
+}
+
+$routeDefinitions = function (RouteCollector $r) {
+    $routes = include('Routes.php');
+    foreach ($routes as $route) {
+        $r->addRoute($route[0], $route[1], $route[2]);
+    }
+};
+
+$dispatcher = \FastRoute\simpleDispatcher($routeDefinitions);
+
+$routeInfo = $dispatcher->dispatch($request->getMethod(), $request->getPath());
+
+switch ($routeInfo[0]) {
+    case Dispatcher::NOT_FOUND:
+        $response->setContent('404 - Page Not Found');
+        $response->setStatusCode(400);
+        break;
+    case Dispatcher::METHOD_NOT_ALLOWED:
+        $response->setContent('405 - Method not allowed');
+        $response->setStatusCode(405);
+        break;
+    case Dispatcher::FOUND:
+        $handler = $routeInfo[1];
+        $vars = $routeInfo[2];
+        call_user_func($handler, $vars);
+        break;
+}
+
+echo $response->getContent();
